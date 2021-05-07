dce6375a9d76020962eadde310d1d422d39f8d66
Rokas <rookas.rudzenskas@gmail.com>
2021-05-07 21:59:22 +0300

    Rokas

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import "./styles/VideoFooter.css";
-import { Button, Avatar } from '@material-ui/core'
-import MusicNoteIcon from '@material-ui/icons/MusicNote'
-import Ticker from 'react-ticker'
-import FavoriteIcon from '@material-ui/icons/Favorite'
-import ModeCommentIcon from '@material-ui/icons/ModeComment'
-import SendIcon from '@material-ui/icons/Send'
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import { Button, Avatar } from '@material-ui/core';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import Ticker from 'react-ticker';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ModeCommentIcon from '@material-ui/icons/ModeComment';
+import SendIcon from '@material-ui/icons/Send';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 
 const VideoFooter = ({ channel, song, likes, shares, avatarSrc }) => {
     return (
@@ -16,6 +16,17 @@ const VideoFooter = ({ channel, song, likes, shares, avatarSrc }) => {
                 <h3>
                     {channel} • <Button>Follow</Button>
                 </h3>
+            </div>
+
+            <div className="videoFooter__ticker">
+                <MusicNoteIcon className="videoFooter__icon" />
+                <Ticker mode="smooth">
+                    {({ index }) => (
+                        <>
+                        <h1>{song}</h1>
+                        </>
+                    )}
+                </Ticker>
             </div>
 
         </div>
